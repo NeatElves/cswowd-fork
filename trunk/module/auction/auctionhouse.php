cb22e6261f85e507e3f979dc4a42ddfc49271c77
deus.ex.13 <deus.ex.13@e451fad0-63ce-11de-bed1-e3a98b22654c>
2009-12-08 15:38:20 +0000

    Исправлено отображение аукциона (без разделения на фракции!)

--- trunk/module/auction/auctionhouse.php
+++ trunk/module/auction/auctionhouse.php
@@ -4,22 +4,6 @@ include_once("include/functions.php");
 $page = intval(@$_REQUEST['page']);
 $type = @$_REQUEST['auction'];
 
-switch ($type) 
-{
-	case "Alliance":	
-	$location="(location=1 or location=2 or location=3)";
-	break;
-	case "Horde":
-	$location="(location=4 or location=5 or location=6)";
-	break;
-	case "Blackwater":
-	$location="(location=7)";
-	default:
-	$type="Blackwater";
-	$location="(location=7)";
-	break;
-}
-
 $rows = $cDB->selectPage($number,
 "SELECT
 `auctionhouse`.`auctioneerguid`,
@@ -32,7 +16,6 @@ $rows = $cDB->selectPage($number,
 `auctionhouse`.`lastbid`,
 `auctionhouse`.`startbid`,
 `auctionhouse`.`deposit`,
-`auctionhouse`.`location`,
 
 (SUBSTRING_INDEX( SUBSTRING_INDEX(`item_instance`.`data` , ' ' , ?d) , ' ' , -1 )+0) AS `itementry`,
 `item_instance`.`data`,
@@ -42,14 +25,12 @@ FROM
    left join
  `item_instance`
   ON `auctionhouse`.`itemguid` = `item_instance`.`guid`
-WHERE
- $location
 LIMIT ?d, ?d",
 ITEM_FIELD_ENTRY+1,
 getPageOffset($page), $config['fade_limit']);
 echo "<TABLE class=report width=500>";
 echo "<TBODY>";
-echo "<TR><TD colspan=4 class=head>";
+/*echo "<TR><TD colspan=4 class=head>";
 echo "<img src=\"images/gold.gif\">";
 echo " <a href=\"?auction=Alliance\" style=\"Text-decoration: none; color:gold;\">Alliance</a>";
 echo " <img src=\"images/gold.gif\">";
@@ -57,7 +38,7 @@ echo " <a href=\"?auction=Horde\" style=\"Text-decoration: none; color:gold;\">H
 echo " <img src=\"images/gold.gif\">";
 echo " <a href=\"?auction=Blackwater\" style=\"Text-decoration: none; color:gold;\">Blackwater</a>";
 echo " <img src=\"images/gold.gif\">";
-echo "</TD></TR>";
+echo "</TD></TR>";*/
 
 if ($rows)
 {
