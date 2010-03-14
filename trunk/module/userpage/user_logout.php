b638e72301f755ed09acf6ce6c33228203c2249d
deus.ex.13 <deus.ex.13@e451fad0-63ce-11de-bed1-e3a98b22654c>
2010-03-14 10:55:15 +0000

    Подготовка к переезду на новые рельсы...

--- trunk/module/userpage/user_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
- //==============================================================
- // Скрипт выхода пользователя
- //==============================================================
- unset($_SESSION['account_id']);
- unset($_SESSION['username']);
- echo '<a href="index.php">'.$lang['user_logout_succes'].'</a>';
- echo "<script language=javascript>setTimeout(\"location.href='index.php'\", 10);</script>";
-?>
